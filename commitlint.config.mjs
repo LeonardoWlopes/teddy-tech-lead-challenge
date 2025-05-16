3f0a0cfdd69898d0fdce0a49d6355e7332bea9f7
Leonardo Lopes <leonardo@buildbox.com.br>
2025-05-15 23:01:00 -0300

    chore: add main config files

---
+++ commitlint.config.mjs
@@ -0,0 +1,12 @@
+export default {
+	extends: ['@commitlint/config-conventional'],
+	rules: {
+		'type-enum': [
+			2,
+			'always',
+			['hotfix', 'feat', 'fix', 'chore', 'style', 'refactor', 'doc', 'wip', 'test', 'revert'],
+		],
+		'subject-case': [2, 'always', 'lower-case'],
+	},
+	ignores: [(message) => message.includes('chore(release):')],
+};
