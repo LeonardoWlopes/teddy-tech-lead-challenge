cd55bcb11a62f57254f3efafaaffb9367e25e089
Leonardo Lopes <leonardo@buildbox.com.br>
2025-05-21 15:43:49 -0300

    feat(ci): remove ci app runner trigger

--- apps/backend/src/infra/modules/app.module.ts
+++ apps/backend/src/infra/modules/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ClientModule } from './client.module';
 import { TypeOrmModule } from '../database/typeorm/typeorm.module';
 
@@ -14,4 +14,14 @@ import { TypeOrmModule } from '../database/typeorm/typeorm.module';
 	controllers: [],
 	providers: [],
 })
-export class AppModule {}
+export class AppModule {
+	constructor(private readonly configService: ConfigService) {
+		console.log({
+			host: configService.get('DB_HOST'),
+			port: configService.get('DB_PORT'),
+			username: configService.get('DB_USERNAME'),
+			password: configService.get('DB_PASSWORD'),
+			database: configService.get('DB_NAME'),
+		});
+	}
+}
